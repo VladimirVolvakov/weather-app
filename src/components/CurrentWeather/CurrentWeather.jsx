71745fe4dbf35fccb847f9814c1d55a0e30cdf75
VladimirVolvakov <dr.volvakov@gmail.com>
2022-07-17 20:45:42 +0300

    feat: add CurrentWeather component markup (hardcoded bottom part of card)

--- src/components/CurrentWeather/CurrentWeather.jsx
+++ src/components/CurrentWeather/CurrentWeather.jsx
@@ -1,16 +1,42 @@
 // Styles:
-import './CurrentWeather.styles.css';
+import './CurrentWeather.css';
 // Image:
 import WeatherIcon from '../../assets/icons/01d.png';
 
 const CurrentWeather = () => {
   return (
-    <div className='current-weather'>
-      <div className="top">
-        <p className='city'>Belgrade</p>
-        <p className='weather-description'>Sunny</p>
+    <div className='current-weather-card'>
+      <div className='card-top-part'>
+        <div>
+          <p className='city'>Donetsk</p>
+          <p className='weather-description'>Sunny</p>
+        </div>
+        <img className='weather-icon' src={WeatherIcon} alt='Weather' />
       </div>
-      <img className='weather-icon' src={WeatherIcon} alt='Weather' />
+      <div className='card-bottom-part'>
+        <p className='temperature'>18 °C</p>
+        <div className='weather-detailed'>
+          <div className='parameter-row'>
+            <span className='parameter-label'>Details</span>
+          </div>
+          <div className='parameter-row'>
+            <span className='parameter-label'>Feels like</span>
+            <span className='parameter-value'>18 °C</span>
+          </div>
+          <div className='parameter-row'>
+            <span className='parameter-label'>Humidity</span>
+            <span className='parameter-value'>20 %</span>
+          </div>
+          <div className='parameter-row'>
+            <span className='parameter-label'>Pressure</span>
+            <span className='parameter-value'>1013 mbar</span>
+          </div>
+          <div className='parameter-row'>
+            <span className='parameter-label'>Wind</span>
+            <span className='parameter-value'>5 m/s</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
