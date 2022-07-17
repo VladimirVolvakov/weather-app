a81fddc6838fa66774d8ee4b9edbc758bad4e31d
VladimirVolvakov <dr.volvakov@gmail.com>
2022-07-17 17:08:30 +0300

    feat: add weather icons

--- src/components/CurrentWeather/CurrentWeather.jsx
+++ src/components/CurrentWeather/CurrentWeather.jsx
@@ -1,5 +1,7 @@
 // Styles:
 import './CurrentWeather.styles.css';
+// Image:
+import WeatherIcon from '../../assets/icons/01d.png';
 
 const CurrentWeather = () => {
   return (
@@ -8,7 +10,7 @@ const CurrentWeather = () => {
         <p className='city'>Belgrade</p>
         <p className='weather-description'>Sunny</p>
       </div>
-      <img className='weather-icon' src='...' alt='Weather' />
+      <img className='weather-icon' src={WeatherIcon} alt='Weather' />
     </div>
   );
 };
