899b88f7fea0b0c459692adaee13165ac2b1f71b
VladimirVolvakov <dr.volvakov@gmail.com>
2022-07-18 11:21:01 +0300

    feat: add data fetching functionality in App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
 // Components:
-import CitySearch from './components/CitySearch/CitySearch';
-import CurrentWeather from './components/CurrentWeather/CurrentWeather';
+import CitySearch from "./components/CitySearch/CitySearch";
+import CurrentWeather from "./components/CurrentWeather/CurrentWeather";
 // Styles:
-import './App.css';
+import "./App.css";
+// Hooks:
+import { useState } from "react";
+// API:
+import { WEATHER_API_URL, WEATHER_API_KEY } from "./API";
 
 function App() {
+  // Set pieces of state:
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [weatherForecast, setWeatherForecast] = useState(null);
+
   const onSearchChangeHandler = (searchData) => {
-    console.log(searchData);
+    // Get location of city:
+    const [latitude, longitude] = searchData.value.split(" ");
+    // Fetch data for current weather from API:
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}`
+    );
+    // Fetch data for weather forecast from API:
+    const weatherForecastFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}`
+    );
+    // Execute fetching:
+    Promise.all([currentWeatherFetch, weatherForecastFetch])
+      .then(async (response) => {
+        const currentWeatherResponse = await response[0].json();
+        const weatherForecastResponse = await response[1].json();
+
+        // Update state:
+        setCurrentWeather({
+          city: searchData.label,
+          ...currentWeatherResponse,
+        });
+        setWeatherForecast({
+          city: searchData.label,
+          ...weatherForecastResponse,
+        });
+      })
+      .catch((error) => console.log(error));
   };
 
   return (
-    <div className='container'>
+    <div className="container">
       <CitySearch onSearchChange={onSearchChangeHandler} />
-      <CurrentWeather />
+      { currentWeather && (
+        <CurrentWeather currentWeatherData={currentWeather} />
+      ) }
     </div>
   );
 }
