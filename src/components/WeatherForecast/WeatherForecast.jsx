2591ee71fd3128454e23f05e4be61328299cad94
VladimirVolvakov <dr.volvakov@gmail.com>
2022-07-20 12:10:25 +0300

    feat: add mediaqueries for WeatherForecast component styles

--- src/components/WeatherForecast/WeatherForecast.jsx
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -24,7 +24,7 @@ const WeatherForecast = ({ weatherForecastData }) => {
                 <div className="daily-forecast">
                   <img className="icon-small" src={`icons/${dailyForecast.weather[0].icon}.png`} alt="daily-weather" />
                   <label className="day-of-week">{weekdaysForForecast[index]}</label>
-                  <label className="weather-description">{dailyForecast.weather[0].description}</label>
+                  <label className="weather-desc">{dailyForecast.weather[0].description}</label>
                   <label className="forecast-temperature value">{Math.round(dailyForecast.main.temp)}°C</label>
                   <label className="feels-like-temperature">Feels like <span className="value">{Math.round(dailyForecast.main.feels_like)}°C</span></label>
                   <label className="humidity">Humidity: <span className="value">{dailyForecast.main.humidity} %</span></label>
@@ -33,7 +33,26 @@ const WeatherForecast = ({ weatherForecastData }) => {
                 </div>
               </AccordionItemButton>
             </AccordionItemHeading>
-            <AccordionItemPanel></AccordionItemPanel>
+            <AccordionItemPanel>
+              <div className="daily-details">
+                <div className="daily-details-item">
+                  <label>Feels like</label>
+                  <label><span className="value">{Math.round(dailyForecast.main.feels_like)}°C</span></label>
+                </div>
+                <div className="daily-details-item">
+                  <label>Humidity</label>
+                  <label><span className="value">{dailyForecast.main.humidity} %</span></label>
+                </div>
+                <div className="daily-details-item">
+                  <label>Pressure</label>
+                  <label><span className="value">{dailyForecast.main.pressure} mbar</span></label>
+                </div>
+                <div className="daily-details-item">
+                  <label>Wind</label>
+                  <label><span className="value">{Math.round(dailyForecast.wind.speed)} m/s</span></label>
+                </div>
+              </div>
+            </AccordionItemPanel>
           </AccordionItem>
         )) }
       </Accordion>
